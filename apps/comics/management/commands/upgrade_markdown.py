83b01f21314af80a4fcfa7ae5c72296a5742036f
Thane Brimhall <thane@seekar.me>
2020-01-04 00:50:02 -0700

    Added one-time upgrade to transcripts for new tagging

--- apps/comics/management/commands/upgrade_markdown.py
+++ apps/comics/management/commands/upgrade_markdown.py
@@ -1,7 +1,10 @@
+import re
+
 from django.core.management.base import BaseCommand, no_translations
 
-from apps.comics.models import Page
+from apps.comics.models import Page, Tag
 
+SPEAKER_EXTRACTION = re.compile(r'\*\*([\w ]+):\*\*')
 
 class Command(BaseCommand):
     """Create or renew an SSL certificate for all configured domains.
@@ -13,5 +16,16 @@ class Command(BaseCommand):
     @no_translations
     def handle(self, *args, **options):
         for page in Page.objects.all():
-            print(page.transcript)
-            return
+            if not page.transcript:
+                continue
+            # Extract all "speakers"
+            matches = SPEAKER_EXTRACTION.findall(page.transcript)
+            for match in matches:
+                tag = Tag.objects.filter(title__iexact=match).select_related('type').first()
+                if tag:
+                    new_syntax = f"<{tag.type.title}:{tag.title}>:"
+                    page.transcript = page.transcript.replace(f"**{match}:**", new_syntax)
+                    page.save()
+                else:
+                    new_syntax = "None"
+                print(match, tag, new_syntax)
